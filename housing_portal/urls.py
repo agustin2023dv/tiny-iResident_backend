2c72fa9ae4797245578662ae2aa4c07feeeee284
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2025-07-11 15:57:21 -0300

    chore: initialize Django project including basic PostgreSQL config

---
+++ housing_portal/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf.urls.static import static
+from housing_portal import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Auth endpoints
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    #  Core API
+    path('api/', include('core.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
